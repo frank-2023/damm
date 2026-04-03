9c65bbb7ac0f2ead5774de9b399f380734e47ed5
frank-2023 <cye384034@gmail.com>
2026-04-03 13:13:08 +0800

    添加固定输出计算

--- rust-sdk/src/quote_exact_out.rs
+++ rust-sdk/src/quote_exact_out.rs
@@ -1,9 +1,11 @@
 use crate::utils::*;
-use anyhow::{ensure, Ok, Result};
+use anyhow::{ensure, Result};
+use std::result::Result::Ok;
 use cp_amm::{
     params::swap::TradeDirection,
     state::{fee::FeeMode, Pool, SwapResult2},
 };
+use cp_amm::state::SimulateSwapResult2;
 
 pub fn get_quote(
     pool: &Pool,
@@ -34,4 +36,57 @@ pub fn get_quote(
     )?;
 
     Ok(swap_result)
+}
+
+pub fn get_out_simulate(
+    old_pool: &Pool,
+    current_timestamp: u64,
+    current_slot: u64,
+    actual_amount_out: u64,
+    a_to_b: bool,
+    has_referral: bool,
+) -> Result<SimulateSwapResult2> {
+    ensure!(actual_amount_out > 0, "amount is zero");
+    let mut pool = old_pool.clone();
+    let current_point = get_current_point(pool.activation_type, current_slot, current_timestamp)?;
+    ensure!(is_swap_enable(&pool, current_point)?, "Swap is disabled");
+
+    let trade_direction = if a_to_b {
+        TradeDirection::AtoB
+    } else {
+        TradeDirection::BtoA
+    };
+
+    let fee_mode = &FeeMode::get_fee_mode(pool.collect_fee_mode, trade_direction, has_referral)?;
+
+    match pool.get_swap_result_from_exact_output(
+        actual_amount_out,
+        fee_mode,
+        trade_direction,
+        current_point,
+    ) {
+        Ok(quote) => {
+            pool.apply_swap_result(&quote,fee_mode,current_timestamp);
+            Ok(SimulateSwapResult2{
+                swap_result2: quote,
+                new_pool: pool,
+                is_update: true,
+            })
+        }
+        Err(e) => std::prelude::rust_2015::Ok(SimulateSwapResult2{
+            swap_result2: SwapResult2{
+                included_fee_input_amount: 0,
+                excluded_fee_input_amount: 0,
+                amount_left: 0,
+                output_amount: 0,
+                next_sqrt_price: 0,
+                trading_fee: 0,
+                protocol_fee: 0,
+                partner_fee: 0,
+                referral_fee: 0,
+            },
+            new_pool: pool,
+            is_update: false,
+        }),
+    }
 }
